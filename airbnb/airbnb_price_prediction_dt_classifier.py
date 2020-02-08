db42cfc545ac83c2d76aa3a2b63798a91cd0e587
Andrew Kogler <andrew.kogler@utexas.edu>
2020-02-07 23:35:45 -0500

    Many updates to model_utils file particularly in plotting model performance metrics and graphs. Also, mostly finished decision_tree model pipeline for Airbnb use-case.

--- airbnb/airbnb_price_prediction_dt_classifier.py
+++ airbnb/airbnb_price_prediction_dt_classifier.py
@@ -9,9 +9,12 @@ from sklearn.model_selection import learning_curve, ShuffleSplit, train_test_spl
 
 from IPython.display import display, Image
 import pydotplus
+import matplotlib.pyplot as plt
+
 from sklearn import tree
 
-from model_utils import plot_hyperparam_search, plot_hyperparam_search_helper, get_train_test_validation_split
+from model_utils import perf_random_search_for_best_hyper_params, plot_opt_model_perf
+from model_utils import plot_complexity_curves_for_hyperparams, plot_learning_curve, plot_learning_curves_helper, store_model
 
 def prune_tree_model(train_features, train_labels, test_features, test_labels):
     clf = DecisionTreeClassifier(criterion='entropy', max_depth=10, min_samples_split=5, min_samples_leaf=10)
@@ -30,129 +33,17 @@ def prune_tree_model(train_features, train_labels, test_features, test_labels):
     		best_clf = [clf]
     return best_clf[-1].ccp_alpha
 
-# Borrowed from sklearn tutorial/example
-def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
-                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
-    """
-    Generate 3 plots: the test and training learning curve, the training
-    samples vs fit times curve, the fit times vs score curve.
-    Parameters
-    ----------
-    estimator : object type that implements the "fit" and "predict" methods
-        An object of that type which is cloned for each validation.
-    title : string
-        Title for the chart.
-    X : array-like, shape (n_samples, n_features)
-        Training vector, where n_samples is the number of samples and
-        n_features is the number of features.
-    y : array-like, shape (n_samples) or (n_samples, n_features), optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
-    axes : array of 3 axes, optional (default=None)
-        Axes to use for plotting the curves.
-    ylim : tuple, shape (ymin, ymax), optional
-        Defines minimum and maximum yvalues plotted.
-    cv : int, cross-validation generator or an iterable, optional
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-          - None, to use the default 5-fold cross-validation,
-          - integer, to specify the number of folds.
-          - :term:`CV splitter`,
-          - An iterable yielding (train, test) splits as arrays of indices.
-        For integer/None inputs, if ``y`` is binary or multiclass,
-        :class:`StratifiedKFold` used. If the estimator is not a classifier
-        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validators that can be used here.
-    n_jobs : int or None, optional (default=None)
-        Number of jobs to run in parallel.
-        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
-        for more details.
-    train_sizes : array-like, shape (n_ticks,), dtype float or int
-        Relative or absolute numbers of training examples that will be used to
-        generate the learning curve. If the dtype is float, it is regarded as a
-        fraction of the maximum size of the training set (that is determined
-        by the selected validation method), i.e. it has to be within (0, 1].
-        Otherwise it is interpreted as absolute sizes of the training sets.
-        Note that for classification the number of samples usually have to
-        be big enough to contain at least one sample from each class.
-        (default: np.linspace(0.1, 1.0, 5))
-    """
-    if axes is None:
-        _, axes = plt.subplots(2, 2, figsize=(20, 5))
-    axes[0].set_title(title)
-    if ylim is not None:
-        axes[0].set_ylim(*ylim)
-    axes[0].set_xlabel("Training examples")
-    axes[0].set_ylabel("Score")
-    train_sizes, train_scores, test_scores, fit_times, _ = \
-        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
-                       train_sizes=train_sizes,
-                       return_times=True)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-    fit_times_mean = np.mean(fit_times, axis=1)
-    fit_times_std = np.std(fit_times, axis=1)
-    # Plot learning curve
-    axes[0].grid()
-    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
-                         train_scores_mean + train_scores_std, alpha=0.1,
-                         color="r")
-    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1,
-                         color="g")
-    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
-                 label="Training score")
-    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
-                 label="Cross-validation score")
-    axes[0].legend(loc="best")
-    # Plot n_samples vs fit_times
-    axes[1].grid()
-    axes[1].plot(train_sizes, fit_times_mean, 'o-')
-    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
-                         fit_times_mean + fit_times_std, alpha=0.1)
-    axes[1].set_xlabel("Training examples")
-    axes[1].set_ylabel("fit_times")
-    axes[1].set_title("Scalability of the model")
-    # Plot fit_time vs score
-    axes[2].grid()
-    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
-    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1)
-    axes[2].set_xlabel("fit_times")
-    axes[2].set_ylabel("Score")
-    axes[2].set_title("Performance of the model")
-    return plt
-
-def plot_learning_curves_helper(model, features, labels):
-	fig, axes = plt.subplots(3, 2, figsize=(10, 15))
-	cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-	plot = plot_learning_curve(model, "Decision Tree Classifier Learning Curves", \
-						features, labels, axes=axes[:, 1], ylim=(0.5, 1.01), \
-							cv=cv, n_jobs=4, train_sizes=np.linspace(.1, 1.0, 10))
-	plt.savefig("airbnb_rental_classifier_tree_learning_curves.png")
-
-def generate_tree_config_diagram(model, feature_names, model_name='base'):
-    output_file_name = 'tree_' + model_name + '.dot'
+def generate_tree_config_diagram(model, feature_names):
+    model_type = model.__class__.__name__
+    output_file_name = model_type + '.dot'
     dot_data = tree.export_graphviz(model, out_file = output_file_name,
                                  feature_names=feature_names, filled   = True
                                     , rounded  = True
                                     , special_characters = True)
     graph = pydotplus.graph_from_dot_file(output_file_name)  
-    graph.write_png('tree_' + model_name + '.png')
+    graph.write_png(model_type + "_diagram.png")
 
-def perform_tree_hyperparameter_sweep(features, labels, problem_name, plot_dir):
-    parameter_search_space = [[{'max_depth': 5}, {'max_depth': 8}, {'max_depth': 12}, {'max_depth': 15}], \
-                                [{'min_samples_split': 100}, {'min_samples_split': 150}, {'min_samples_split': 200}, {'min_samples_split': 250}], \
-                                [{'min_samples_leaf': 10}, {'min_samples_leaf': 25}, {'min_samples_leaf': 35}, {'min_samples_leaf': 50}]]
-    clf = DecisionTreeClassifier()
-    for param_set in parameter_search_space:
-        plot_hyperparam_search_helper(clf, features, labels, param_set, problem_name, plot_dir)
-
-def sweep_hyperparameter_space_and_plot_complexity_curves(features, labels, problem_name, plot_dir):
+def sweep_hyperparameter_space_and_plot_complexity_curves(clf, features, labels, problem_name, plot_dir):
     parameter_search_space = [[{'max_depth': 5}, {'max_depth': 8}, {'max_depth': 12}, {'max_depth': 15}, \
                                              {'max_depth': 20}, {'max_depth': 25}, {'max_depth': 30}], \
                                 [{'min_samples_split': 25}, {'min_samples_split': 50}, {'min_samples_split': 100}, \
@@ -160,48 +51,49 @@ def sweep_hyperparameter_space_and_plot_complexity_curves(features, labels, prob
                                 [{'min_samples_leaf': 50}, {'min_samples_leaf': 100}, {'min_samples_leaf': 150}, {'min_samples_leaf': 200}, \
                                 {'min_samples_leaf': 250}, {'min_samples_leaf': 325}, {'min_samples_leaf': 400}, {'min_samples_leaf': 500}]]
     for hyperparam_range in parameter_search_space:
-        plot_complexity_curves_for_hyperparams(c, train_features,train_labels, hyperparam_range, \
-                                                'Airbnb Rental Price Classification', '../../omscs_ml_project1/airbnb/plots/')
+        plot_complexity_curves_for_hyperparams(clf, train_features,train_labels, hyperparam_range, \
+                                                problem_name, plot_dir)
 
 
 
-features_and_labels_dataset_path = sys.argv[1]
+training_dataset_path = sys.argv[1]
 
-plot_dir = sys.argv[2]
+test_dataset_path = sys.argv[2]
 
-airbnb_features_data_df = pd.read_csv(features_and_labels_dataset_path)
+plot_dir = sys.argv[3]
 
-sampled_dataset = airbnb_features_data_df
+problem_name = 'airbnb_rental_price_classification'
 
-feature_cols = [x for x in sampled_dataset.columns if x != 'price' and x != 'expensive']
+train = pd.read_csv(training_dataset_path)
 
-# get_train_test_validation_split(feature_df, train_set_size=0.6, validation_set_size=0.1)
-train, validation, test = get_train_test_validation_split(sampled_dataset, train_set_size=0.6, validation_set_size=0.2)
+test = pd.read_csv(test_dataset_path)
+
+feature_cols = [x for x in train.columns if x != 'price' and x != 'expensive']
+
 train_features, train_labels = train[[col for col in feature_cols]], train[['expensive']]
-validation_features, validation_labels = validation[[col for col in feature_cols]], validation[['expensive']]
 test_features, test_labels = test[[col for col in feature_cols]], test[['expensive']]
 
 clf = DecisionTreeClassifier()
 
-#pruned_ccp_alpha = prune_tree_model(train_features, train_labels, validation_features, validation_labels)
+sweep_hyperparameter_space_and_plot_complexity_curves(clf, train_features, train_labels, problem_name, plot_dir)
 
-# perform_tree_hyperparameter_sweep(train_features, train_labels, problem_name='Airbnb Rental Price Classification', plot_dir=plot_dir)
+param_dist = {'max_depth': [5, 8, 12, 15, 20, 25, 30], 'min_samples_split': [25, 50, 100, 150, 200, 300, 400, 500], \
+              'min_samples_leaf': [50, 100, 150, 200, 250, 325, 400, 500]}
 
-"""
-pruned_clf = DecisionTreeClassifier(criterion='entropy', max_depth=10, min_samples_split=5, min_samples_leaf=10, ccp_alpha=pruned_ccp_alpha)
+opt_param_set_from_random_search = perf_random_search_for_best_hyper_params(clf, train_features, train_labels, param_dist, n_iter_search=20)
 
-pruned_clf.fit(train_features, train_labels)
+clf.set_params(**opt_param_set_from_random_search)
 
-print(pruned_clf.score(train_features, train_labels))
+scoring_metric = 'f1'
 
-prepruned_clf = DecisionTreeClassifier(criterion='entropy', max_depth=5, max_features="log2", min_samples_split=200, min_samples_leaf=35, max_leaf_nodes=125)
+plot_learning_curves_helper(clf, train_features, train_labels, scoring_metric, plot_dir, problem_name)
 
-prepruned_clf.fit(train_features, train_labels)
-print(prepruned_clf.score(test_features, test_labels))
+clf.fit(train_features, train_labels)
 
-print(pruned_clf.feature_importances_)
-"""
+generate_tree_config_diagram(clf, feature_cols)
 
-prepruned_clf = DecisionTreeClassifier(max_depth=8, min_samples_split=200, min_samples_leaf=35)
+plot_opt_model_perf(clf, test_features, test_labels, [0, 1], problem_name, plot_dir)
 
+model_path = "models/airbnb_dt_classifier_model.joblib"
 
+store_model(clf, model_path)
--- airbnb/model_utils.py
+++ airbnb/model_utils.py
@@ -1,10 +1,15 @@
 import sys
 
+import time
+
+from joblib import dump, load
+
 import pandas as pd
 import numpy as np
 
-from sklearn.metrics import confusion_matrix, roc_auc_score, classification_report
-from sklearn.model_selection import learning_curve, ShuffleSplit, KFold
+from sklearn.metrics import confusion_matrix, roc_auc_score, classification_report, f1_score, roc_curve, auc, plot_confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, precision_recall_curve, plot_precision_recall_curve
+from sklearn.model_selection import learning_curve, ShuffleSplit, KFold, RandomizedSearchCV
 from sklearn.base import clone
 
 from IPython.display import display, Image
@@ -14,99 +19,124 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.parasite_axes import host_subplot
 from mpl_toolkits.axisartist.axislines import Axes
 
+def store_model(model, model_path):
+	dump(model, model_path) 
+
 def get_train_test_validation_split(feature_df, train_set_size=0.6, validation_set_size=0.1):
     train, validate, test = np.split(feature_df.sample(frac=1), [int(
         train_set_size * len(feature_df)), int((train_set_size + validation_set_size) * len(feature_df))])
-    #X_train, X_test, y_train, y_test = train_test_split(feature_df[feature_cols], feature_df['loan_outcome'], test_size=0.25)
     return train, validate, test
 
-def plot_hyperparam_search(estimator, title_base_str, X, y, axes=None, ylim=None, cv=None,
-							n_jobs=None, train_sizes=np.linspace(.1, 1.0, 10), params_list=None):
-	if axes is None:
-		fig, axes = plt.subplots(2, 2, figsize=(15, 5))
-		fig.subplots_adjust(hspace=0.6, wspace=0.2)
-	first_title_str = title_base_str + "\n where " + list(params_list[0].keys())[0] + " = " + str(list(params_list[0].values())[0])
-	second_title_str = title_base_str + "\n where " + list(params_list[1].keys())[0] + " = " + str(list(params_list[1].values())[0])
-	third_title_str = title_base_str + "\n where " + list(params_list[2].keys())[0] + " = " + str(list(params_list[2].values())[0])
-	fourth_title_str = title_base_str + "\n where " + list(params_list[3].keys())[0] + " = " + str(list(params_list[3].values())[0])
-	axes[0, 0].set_title(first_title_str)
-	axes[0, 1].set_title(second_title_str)
-	axes[1, 0].set_title(third_title_str)
-	axes[1, 1].set_title(fourth_title_str)
-	if ylim is not None:
-		axes[0, 0].set_ylim(*ylim)
-	axes[0, 0].set_xlabel("Training examples")
-	axes[0, 0].set_ylabel("Score")
-	axes[0, 1].set_xlabel("Training examples")
-	axes[0, 1].set_ylabel("Score")
-	axes[1, 0].set_xlabel("Training examples")
-	axes[1, 0].set_ylabel("Score")
-	axes[1, 1].set_xlabel("Training examples")
-	axes[1, 1].set_ylabel("Score")
-	train_scores_avg_list = []
-	train_scores_std_list = []
-	test_scores_avg_list = []
-	test_scores_std_list = []
-	for param_setting in params_list:
-		estimator.set_params(**param_setting)
-		train_sizes, train_scores, test_scores, fit_times, _ = \
-			learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
-								train_sizes=train_sizes,
-								return_times=True)
-		train_scores_mean = np.mean(train_scores, axis=1)
-		train_scores_std = np.std(train_scores, axis=1)
-		test_scores_mean = np.mean(test_scores, axis=1)
-		test_scores_std = np.std(test_scores, axis=1)
-		train_scores_avg_list.append(train_scores_mean)
-		train_scores_std_list.append(train_scores_std)
-		test_scores_avg_list.append(test_scores_mean)
-		test_scores_std_list.append(test_scores_std)
-	#axes[0, 0].grid()
-	axes[0, 0].fill_between(train_sizes, train_scores_avg_list[0] - train_scores_std[0],
-							train_scores_avg_list[0] + train_scores_std[0], alpha=0.1,
-							color="r")
-	axes[0, 0].fill_between(train_sizes, test_scores_avg_list[0] - test_scores_std[0],
-							test_scores_avg_list[0] + test_scores_std[0], alpha=0.1,
-							color="g")
-	axes[0, 1].fill_between(train_sizes, train_scores_avg_list[1] - train_scores_std[1],
-							train_scores_avg_list[1] + train_scores_std[1], alpha=0.1,
-							color="r")
-	axes[0, 1].fill_between(train_sizes, test_scores_avg_list[1] - test_scores_std[1],
-						test_scores_avg_list[1] + test_scores_std[1], alpha=0.1,
-						color="g")
-	axes[1, 0].fill_between(train_sizes, train_scores_avg_list[2] - train_scores_std[2],
-						train_scores_avg_list[2] + train_scores_std[2], alpha=0.1,
-						color="r")
-	axes[1, 0].fill_between(train_sizes, test_scores_avg_list[2] - test_scores_std[2],
-						test_scores_avg_list[2] + test_scores_std[2], alpha=0.1,
-						color="g")
-	axes[1, 1].fill_between(train_sizes, train_scores_avg_list[3] - train_scores_std[3],
-						train_scores_avg_list[3] + train_scores_std[3], alpha=0.1,
-						color="r")
-	axes[1, 1].fill_between(train_sizes, test_scores_avg_list[3] - test_scores_std[3],
-						test_scores_avg_list[3] + test_scores_std[3], alpha=0.1,
-						color="g")
-	axes[0, 0].plot(train_sizes, train_scores_avg_list[0], 'o-', color="r",
-					label="Training score")
-	axes[0, 0].plot(train_sizes, test_scores_avg_list[0], 'o-', color="g",
-					label="Cross-validation score")
-	axes[0, 1].plot(train_sizes, train_scores_avg_list[1], 'o-', color="r",
-					label="Training score")
-	axes[0, 1].plot(train_sizes, test_scores_avg_list[1], 'o-', color="g",
-					label="Cross-validation score")
-	axes[1, 0].plot(train_sizes, train_scores_avg_list[2], 'o-', color="r",
-					label="Training score")
-	axes[1, 0].plot(train_sizes, test_scores_avg_list[2], 'o-', color="g",
-					label="Cross-validation score")
-	axes[1, 1].plot(train_sizes, train_scores_avg_list[3], 'o-', color="r",
-					label="Training score")
-	axes[1, 1].plot(train_sizes, test_scores_avg_list[3], 'o-', color="g",
-					label="Cross-validation score")
-	axes[0, 0].legend(loc="best")
-	axes[0, 1].legend(loc="best")
-	axes[1, 0].legend(loc="best")
-	axes[1, 1].legend(loc="best")
-	return plt
+def plot_conf_matrix_wrapper(model, features, labels, label_names, ax):
+	plot_obj = plot_confusion_matrix(model, features, labels, labels=label_names, normalize='true', ax=ax)
+	#model_type = model.__class__.__name__
+
+# Borrowed from sklearn tutorial/example
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5), scoring_metric=None):
+    """
+    Generate 3 plots: the test and training learning curve, the training
+    samples vs fit times curve, the fit times vs score curve.
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+    title : string
+        Title for the chart.
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+    axes : array of 3 axes, optional (default=None)
+        Axes to use for plotting the curves.
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 5-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+    n_jobs : int or None, optional (default=None)
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+    train_sizes : array-like, shape (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
+        fraction of the maximum size of the training set (that is determined
+        by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
+        Note that for classification the number of samples usually have to
+        be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    if axes is None:
+        _, axes = plt.subplots(2, 2, figsize=(20, 5))
+    axes[0, 0].set_title(title)
+    if ylim is not None:
+        axes[0, 0].set_ylim(*ylim)
+    axes[0, 0].set_xlabel("Training examples")
+    axes[0, 0].set_ylabel("Score (F1)")
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, scoring=scoring_metric, n_jobs=n_jobs, \
+                       train_sizes=train_sizes, \
+                       return_times=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+    # Plot learning curve
+    axes[0, 0].grid()
+    axes[0, 0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0, 0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0, 0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0, 0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes[0, 0].legend(loc="best")
+    # Plot n_samples vs fit_times
+    axes[0, 1].grid()
+    axes[0, 1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[0, 1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    axes[0, 1].set_xlabel("Training examples")
+    axes[0, 1].set_ylabel("fit_times")
+    axes[0, 1].set_title("Scalability of the model")
+    # Plot fit_time vs score
+    axes[1, 0].grid()
+    axes[1, 0].plot(fit_times_mean, test_scores_mean, 'o-')
+    axes[1, 0].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1)
+    axes[1, 0].set_xlabel("fit_times")
+    axes[1, 0].set_ylabel("Score (F1)")
+    axes[1, 0].set_title("Performance of the model")
+    return plt
+
+def plot_learning_curves_helper(model, features, labels, scoring_metric, plot_dir, problem_name):
+	model_type = model.__class__.__name__
+	fig, axes = plt.subplots(2, 2, figsize=(10, 15))
+	cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+	plot = plot_learning_curve(model, model_type + " Learning Curves", \
+						features, labels, axes=axes, ylim=(0.5, 1.01), \
+							cv=cv, n_jobs=4, train_sizes=np.linspace(.1, 1.0, 10), \
+							scoring_metric=scoring_metric)
+	plt.savefig(plot_dir + problem_name + "_" + model_type + "_learning_curves.png")
 
 def plot_model_complexity_graph(estimator, title_base_str, X, y, axes=None, ylim=None, cv=None, params_list=None):
 	validation_scores_list = []
@@ -161,6 +191,58 @@ def plot_complexity_curves_for_hyperparams(model, features, labels, params_list,
 	param_name = list(params_list[0].keys())[0]
 	plt.savefig(plot_dir + problem_name + "_" + model_type + "_" + param_name + "_param_complexity_curves.png")
 
-def plot_opt_model_perf(model, features, labels, problem_name, plot_dir):
 
+def plot_opt_model_perf(model, features, labels, label_names, problem_name, plot_dir, axes=None):
+	model_type = model.__class__.__name__
+	if axes is None:
+		fig, axes = plt.subplots(2, 2, figsize=(10, 5))
+		fig.subplots_adjust(hspace=0.3, wspace=0.2)
+	axes[0, 0].set_title(model_type + " ROC Curve")
+	fpr, tpr, _ = roc_curve(labels, model.predict_proba(features)[:, 1])
+	roc_auc = auc(fpr, tpr)
+	lw = 2
+	axes[0, 0].plot(fpr, tpr, color='darkorange',
+				lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+	axes[0, 0].plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+	axes[0, 0].set_xlim([0.0, 1.0])
+	axes[0, 0].set_ylim([0.0, 1.05])
+	axes[0, 0].set_xlabel('False Positive Rate')
+	axes[0, 0].set_ylabel('True Positive Rate')
+	axes[0, 0].legend(loc="lower right")
+	conf_matrix_ax = axes[0, 1]
+	plot_conf_matrix_wrapper(model, features, labels, label_names, conf_matrix_ax)
+	axes[1, 0].axis('off')
+	axes[1, 0].grid(b=None)
+	model_predictions = model.predict(features)
+	model_pred_probs = model.predict_proba(features)
+	model_acc = round(accuracy_score(labels, model_predictions), 5)
+	precision = round(precision_score(labels, model_predictions), 5)
+	recall = round(recall_score(labels, model_predictions), 5)
+	f1 = round(f1_score(labels, model_predictions), 5)
+	model_perf_array = [[str(model_acc), str(precision), str(recall), str(f1), str(round(roc_auc, 5))]]
+	axes[1, 0].table(cellText=model_perf_array, cellLoc='center', rowLabels=None, colLabels=['Accuracy', 'Precision', 'Recall', 'F1-Score', 'AUC Score'], colLoc='center', loc='center')
+	prec_recall_curve_ax = axes[1, 1]
+	plot_precision_recall_curve(model, features, labels, ax=prec_recall_curve_ax)
+	plt.savefig(plot_dir + problem_name + "_" + model_type + "_test_set_performance.png")
+
+# Utility function to report best scores
+def report(results, n_top=3):
+	for i in range(1, n_top + 1):
+		candidates = np.flatnonzero(results['rank_test_score'] == i)
+		for candidate in candidates:
+			print("Model with rank: {0}".format(i))
+			print("Mean validation score: {0:.3f} (std: {1:.3f})"
+				.format(results['mean_test_score'][candidate],
+					results['std_test_score'][candidate]))
+			print("Parameters: {0}".format(results['params'][candidate]))
+			print("")
+
+def perf_random_search_for_best_hyper_params(clf, features, labels, param_dist, n_iter_search):
+	random_search = RandomizedSearchCV(clf, param_distributions=param_dist, n_iter=n_iter_search, scoring='f1')
+	start = time.time()
+	random_search.fit(features, labels)
+	print("RandomizedSearchCV took %.2f seconds for %d candidates"
+		" parameter settings." % ((time.time() - start), n_iter_search))
+	report(random_search.cv_results_)
+	return random_search.best_params_
 
